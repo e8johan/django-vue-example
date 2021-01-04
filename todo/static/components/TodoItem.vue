11c7c6bffdfaaf6b8fc810bc049c76cfcc6c9919
Johan Thelin <hello@e8johan.se>
2021-01-04 22:46:42 +0100

    bug: relative path to static resource

--- todo/static/components/TodoItem.vue
+++ todo/static/components/TodoItem.vue
@@ -3,7 +3,7 @@
         <span v-if="item.done" v-on:click="$emit('clear', item.id);">[x]</span>
         <span v-else="" v-on:click="$emit('check', item.id);">[&nbsp;]</span>
         {{ item.text }}
-        <img src="/static/images/trashcan.png" v-on:click="$emit('delete', item.id);" />
+        <img src="../images/trashcan.png" v-on:click="$emit('delete', item.id);" />
     </p>
 </template>
 
